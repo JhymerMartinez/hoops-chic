7edd6fd7852f459913eb26fa720b0f7c2331daf4
Jhymer Martínez <jhmartinez1991@gmail.com>
2024-02-17 10:16:25 -0500

    feat: update Catalog Card

--- src/components/CatalogCard/CatalogCard.tsx
+++ src/components/CatalogCard/CatalogCard.tsx
@@ -1,6 +1,19 @@
 import { ICatalogCardProps } from "@/interfaces";
 import { StyledPaper } from "./CatalogCard.styles";
+import Image from "next/image";
 
 export const CatalogCard = (props: ICatalogCardProps) => {
-  return <StyledPaper {...props} elevation={0} />;
+  return (
+    <StyledPaper {...props} elevation={0}>
+      <Image
+        src={props.data.images[0].src}
+        alt={props.data.images[0].alt}
+        width={100}
+        height={100}
+      />
+      <h3>{props.data.name}</h3>
+      <p>{props.data.description}</p>
+      <p>{props.data.price}</p>
+    </StyledPaper>
+  );
 };
