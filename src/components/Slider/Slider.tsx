5e46cdff28313a2282e2ca12b487cf9d8bc67e05
Jhymer Martínez <jhmartinez1991@gmail.com>
2024-03-02 09:58:49 -0500

    feat: add an image carousel on the header

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,4 +1,11 @@
 import { Box } from "@mui/material";
+import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { EffectFade, Navigation, Pagination } from "swiper/modules";
 
 export const Slider = () => {
   return (
@@ -9,7 +16,49 @@ export const Slider = () => {
         height: "500px",
       }}
     >
-      Slider
+      <Swiper
+        spaceBetween={30}
+        effect={"fade"}
+        navigation={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[EffectFade, Navigation, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <Image
+            fill
+            alt="Nature"
+            src="https://picsum.photos/id/15/1280/720"
+            loading="lazy"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            fill
+            alt="Nature"
+            src="https://picsum.photos/id/29/1280/720"
+            loading="lazy"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            fill
+            alt="Nature"
+            src="https://picsum.photos/id/347/1280/720"
+            loading="lazy"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            fill
+            alt="Nature"
+            src="https://picsum.photos/id/926/1280/720"
+            loading="lazy"
+          />
+        </SwiperSlide>
+      </Swiper>
     </Box>
   );
 };
